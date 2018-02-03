425f52070410d41ceeb60ec93403ca363478eca9
Steven Huynh <stevenhuynh@ste.vin>
2018-02-02 19:05:41 -0500

    Made Node.getPossibleNodes() from Node.findBestMove()
    Originally, Node.findBestMove() would traverse through a board's array
    to find valid moves, then create child boards that had those moves
    performed. I moved that functionality to Node.getPossibleNodes().
    
    Node.getPossibleNodes() will probably be useful for the move decision
    component of the minimax algorithm.

--- src/Node.java
+++ src/Node.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.List;
 
 public class Node {
@@ -27,7 +28,7 @@ public class Node {
         board = b;
     }
 
-
+    /*
     static int findBestMove(Node n){
         if(n.depth == 0 || n.board.findSequences(n.maxPlayerColor, 5) > 0 || n.board.findSequences(n.minPlayerColor, 5) > 0){
             if(n.isMaxPlayer)
@@ -77,7 +78,58 @@ public class Node {
             return var;
         }
     }
-
-
-
+    */
+        
+    static int findBestMove(Node n){
+        if(n.depth == 0 || n.board.findSequences(n.maxPlayerColor, 5) > 0 || n.board.findSequences(n.minPlayerColor, 5) > 0){
+            if(n.isMaxPlayer)
+                n.HeuristicVal = n.board.getHeuristic(n.maxPlayerColor) - n.board.getHeuristic(n.minPlayerColor);
+            else
+                n.HeuristicVal = n.board.getHeuristic(n.minPlayerColor) - n.board.getHeuristic(n.maxPlayerColor);
+            return n.HeuristicVal;
+        }
+        if(n.isMaxPlayer){
+            int var = Integer.MIN_VALUE;
+            HashSet<Node> possibleNodeSet = getPossibleNodes(n);
+            for (Node possibleNode : possibleNodeSet) {
+                n.children.add(possibleNode);
+                var = Math.max(var, findBestMove(possibleNode));
+                n.min = Math.max(n.min, var);
+                if(n.min <= n.max)
+                    return var;
+            }
+            return var;
+        }
+        else{
+            int var = Integer.MAX_VALUE;
+            HashSet<Node> possibleNodeSet = getPossibleNodes(n);
+            for (Node possibleNode : possibleNodeSet) {
+                n.children.add(possibleNode);
+                var = Math.max(var, findBestMove(possibleNode));
+                n.min = Math.max(n.min, var);
+                if(n.min <= n.max)
+                    return var;
+            }
+            return var;
+        }
+    }
+    
+    static HashSet<Node> getPossibleNodes(Node n) {
+    		HashSet<Node> possibleNodes = new HashSet<Node>();
+    	
+        for(int i = 0; i < DIMS; i++){
+            for(int j = 0; j < DIMS; j++){
+                if(!n.board.isValid(j, i))
+                    continue;
+                Move lastM = new Move(j, i);
+                Board newBoard = new Board(n.board);
+                newBoard.placePiece(lastM, n.isMaxPlayer?n.maxPlayerColor:n.minPlayerColor);
+                Node newN = new Node(0, n.depth - 1, n.max, n.min, n.maxPlayerColor, n.minPlayerColor, !n.isMaxPlayer, lastM, n, newBoard);
+                
+                possibleNodes.add(newN);
+            }
+        }
+        
+        return possibleNodes;
+    }
 }
